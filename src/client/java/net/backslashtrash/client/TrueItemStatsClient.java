b662f230ec7fe4f55220220b941e3f045aae734a
Jiawei Tang <jiaweit417@gmail.com>
2026-09-05 12:07:55 -0400

    Initial commit

---
+++ src/client/java/net/backslashtrash/client/TrueItemStatsClient.java
@@ -0,0 +1,10 @@
+package net.backslashtrash.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrueItemStatsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
